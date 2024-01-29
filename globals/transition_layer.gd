14e7b3e367e3102aa84f6a78c629f81332ff2c5d
stefanolago <jafarrolo@gmail.com>
2024-01-29 23:36:00 +0100

    Update transition_layer.gd

--- globals/transition_layer.gd
+++ globals/transition_layer.gd
@@ -9,3 +9,12 @@ func change_scene(target: PackedScene) -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(target)
 	animation_player.play_backwards("fade_to_black")
+
+
+func change_scene_to_file(target: String) -> void:
+	animation_player.play("fade_to_black")
+	get_tree().paused = true
+	await animation_player.animation_finished
+	get_tree().paused = false
+	get_tree().change_scene_to_file(target)
+	animation_player.play_backwards("fade_to_black")
