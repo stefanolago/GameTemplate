a61f5c2d55a497db653972e45c47b5c1b0c40462
stefanolago <jafarrolo@gmail.com>
2024-02-01 10:43:48 +0100

    Implemented pause screen with blur

---
+++ godot/globals/pause_screen.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+
+@export var blur_amount_when_paused: float = 2.0
+
+@onready var options_menu: Control = $OptionsMenu
+@onready var shader_material: ShaderMaterial = ($BlurringBackground as ColorRect).material
+
+var mouse_mode_before_pause: Input.MouseMode = Input.MouseMode.MOUSE_MODE_VISIBLE 
+var game_is_paused: bool = false:
+	set(value):
+		game_is_paused = value
+		if game_is_paused:
+			mouse_mode_before_pause = Input.mouse_mode
+		else:
+			Input.mouse_mode = mouse_mode_before_pause
+
+
+		options_menu.visible = game_is_paused
+		get_tree().paused = game_is_paused
+		shader_material.set_shader_parameter("lod", blur_amount_when_paused if game_is_paused else 0.0)
+
+
+func _ready() -> void:
+	options_menu.visible = false
+	shader_material.set_shader_parameter("lod", 0.0)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		var current_scene_name: String = get_tree().current_scene.name
+		if current_scene_name == "StartMenu" or current_scene_name == "StartScreen":
+			return
+		game_is_paused = !game_is_paused
--- godot/scenes/_options_menu/options_menu.gd
+++ godot/scenes/_options_menu/options_menu.gd
@@ -2,6 +2,22 @@ extends Control
 
 signal close_option_menu
 
+## is it working?
+@export var show_as_pause_screen: bool = false
+
+
+@onready var back_button: Button = $BackButton
+@onready var quit_button: Button = $QuitButton
+
+func _ready() -> void:
+	if show_as_pause_screen:
+		back_button.visible = false
+		quit_button.visible = true
+	else:
+		back_button.visible = true
+		quit_button.visible = false
+
+
 func _on_back_button_pressed() -> void:
 	FMODRuntime.play_one_shot_id(FMODGuids.Events.UI_BACK)
 	close_option_menu.emit()
@@ -9,3 +25,7 @@ func _on_back_button_pressed() -> void:
 
 func _on_back_button_mouse_entered() -> void:
 	FMODRuntime.play_one_shot_id(FMODGuids.Events.UI_HOVER)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
